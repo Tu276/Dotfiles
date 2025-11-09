89ce2de5943d02fc5fee2922bfa332dd4c0ede03
Tu276 <tu276nathan@gmail.com>
2025-11-09 19:04:22 +0300

    updates

--- setup.sh
+++ setup.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
+
 echo "setting up  dotfiles"
 git clone https://github.com/Tu276.dotfiles.git ~/dotfiles
 ln -sf ~/dotfiles/.bashrc ~/.bashrc
+ln -sf ~/dotfiles/.bash_aliases ~/.bash_aliases
+
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
-source ~/.bashrc
+ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
+ls -sf ~/dotfiles/.inputrc ~/.inputrc
+
+echo " Done! Run 'source ~/.bashrc' to reload your shell"
 echo "start of sea passage"
